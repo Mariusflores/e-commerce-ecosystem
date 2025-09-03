7914ab7aea0fc73577126f34c63f752707475986
Marius Flores Dalseng <mfloresdal@gmail.com>
2025-09-03 10:09:04 +0200

    Initial Commit

---
+++ src/main/java/org/example/ecommerceecosystem/ECommerceEcosystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.ecommerceecosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceEcosystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceEcosystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/ecommerceecosystem/ECommerceEcosystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ecommerceecosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceEcosystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

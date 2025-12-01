2a92ed17f4946d3c0d378e85cd1ee14f2d9bd6d8
Marius Flores Dalseng <mfloresdal@gmail.com>
2025-12-01 22:00:11 +0100

    removed unused imports

--- product-service/src/main/java/org/example/productservice/repository/ProductRepository.java
+++ product-service/src/main/java/org/example/productservice/repository/ProductRepository.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends MongoRepository<Product, String> {
-    Optional<Product> findBySkuCode(String skuCode);
     boolean existsBySkuCode(String skuCode);
     List<Product> findBySkuCodeIn(List<String> skuCodes);  // For batch queries
 }

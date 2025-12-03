4252732c84ffa327ca735a208d217caf776a1b66
Marius Flores Dalseng <mfloresdal@gmail.com>
2025-12-03 18:07:46 +0100

    Publish Payment event

--- payments-service/src/main/java/org/example/paymentsservice/service/PaymentService.java
+++ payments-service/src/main/java/org/example/paymentsservice/service/PaymentService.java
@@ -1,9 +1,15 @@
 package org.example.paymentsservice.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.domain.dto.events.OrderPlacedEvent;
+import org.example.domain.dto.events.PaymentCompletedEvent;
+import org.example.domain.dto.events.PaymentFailedEvent;
+import org.example.paymentsservice.messaging.PaymentEventPublisher;
+import org.example.paymentsservice.models.Payment;
 import org.example.paymentsservice.repository.PaymentRepository;
+import org.example.paymentsservice.simulation.PaymentSimulationResult;
 import org.example.paymentsservice.simulation.PaymentSimulator;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +20,54 @@ public class PaymentService {
 
     private final PaymentRepository paymentRepository;
     private final PaymentSimulator simulator;
+    private final PaymentEventPublisher eventPublisher;
 
+    @Transactional
     public void processPayment(OrderPlacedEvent order) {
+        PaymentSimulationResult paymentResult = simulator.simulatePayment(
+                order.getTotalAmount(),
+                order.getPaymentMethod()
+        );
 
+        Payment payment = Payment.builder()
+                .orderNumber(order.getOrderNumber())
+                .customerId(order.getCustomerId())
+                .amount(order.getTotalAmount())
+                .paymentMethod(order.getPaymentMethod())
+                .paymentStatus(paymentResult.getPaymentStatus())
+                .transactionId(paymentResult.getTransactionId())
+                .failureReason(paymentResult.getFailureReason())
+                .build();
 
+        paymentRepository.save(payment);
+
+        // Publish event
+        switch (payment.getPaymentStatus()) {
+            case COMPLETED -> eventPublisher.publishPaymentCompletedEvent(mapToPaymentCompletedEvent(payment));
+            case FAILED -> eventPublisher.publishPaymentFailedEvent(mapToPaymentFailedEvent(payment));
+        }
+
+    }
+
+    private PaymentFailedEvent mapToPaymentFailedEvent(Payment payment) {
+        return PaymentFailedEvent.builder()
+                .orderNumber(payment.getOrderNumber())
+                .transactionId(payment.getTransactionId())
+                .amount(payment.getAmount())
+                .paymentMethod(payment.getPaymentMethod())
+                .failureReason(payment.getFailureReason())
+                .failedAt(payment.getCreatedDate())
+
+                .build();
+    }
+
+    private PaymentCompletedEvent mapToPaymentCompletedEvent(Payment payment) {
+        return PaymentCompletedEvent.builder()
+                .orderNumber(payment.getOrderNumber())
+                .transactionId(payment.getTransactionId())
+                .amount(payment.getAmount())
+                .paymentMethod(payment.getPaymentMethod())
+                .completedAt(payment.getCreatedDate())
+                .build();
     }
 }
--- payments-service/src/main/java/org/example/paymentsservice/simulation/PaymentSimulator.java
+++ payments-service/src/main/java/org/example/paymentsservice/simulation/PaymentSimulator.java
@@ -35,7 +35,6 @@ public class PaymentSimulator {
                 .failureReason(status ==
                         PaymentStatus.FAILED ? simulateFailureReason() : null
                 )
-                .processedAt(LocalDateTime.now())
                 .build();
 
     }
